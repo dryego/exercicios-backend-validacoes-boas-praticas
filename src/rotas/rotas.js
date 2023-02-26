f41f444a2a880e28d45a18e01c89a59be0d46b0b
dryego barbosa <dryego.lisboa@gmail.com>
2023-02-26 09:52:50 -0300

    rota detalhar usuario.

--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { atualizarUsuario } = require('../controladores/atualizarUsuario');
 const { cadastroUsuario } = require('../controladores/cadastroUsuario');
+const { detralarUsuario } = require('../controladores/detalharUsuario');
 const login = require('../controladores/fazerLogin');
 const { validadorToken } = require('../intermediarios/validandoToken');
 
@@ -11,6 +12,7 @@ rotas.post('/login', login)
 
 rotas.use(validadorToken)
 
+rotas.get('/usuario', detralarUsuario);
 rotas.put('/usuario', atualizarUsuario)
 
 module.exports = rotas
