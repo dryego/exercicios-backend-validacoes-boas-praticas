099bd8bcb36410c9faa5b6229565264f91033389
dryego barbosa <dryego.lisboa@gmail.com>
2023-02-28 09:37:21 -0300

    adicionado controlador, detalhar uma rota do usuario logado buscando pelo id.

--- src/controladores/detalheTransacaoUsuarioLogado.js
+++ src/controladores/detalheTransacaoUsuarioLogado.js
@@ -1,19 +1,26 @@
 const pool = require("../conexão/conexao")
 
 const detalheTransacaoUsuarioLogado = async (req, res) => {
-    //try {
-    const { id } = req.usuario;
+    const { id } = req.params;
+    const { usuario } = req
 
-    if (!id) {
-        return res.status(400).json({ mensagem: 'Transação não encontrada.' })
+    try {
+
+        const detalhesTransacoes = await pool.query('select * from transacoes where usuario_id = $1', [usuario.id]);
+
+        const buscaIdTransacao = detalhesTransacoes.rows.find(busca => busca.id == id)
+
+        // const buscarIdCategoria = detalhesTransacoes.rows.find(busca => busca.categoria_id);
+
+        if (buscaIdTransacao === undefined) {
+            return res.status(400).json({ mensagem: 'Transação não encontrada' });
+        }
+
+        return res.status(200).json(buscaIdTransacao);
+
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'Erro interno.' })
     }
-
-    const detalhesTransacoes = await pool.query('select from transacoes where usuario_id = $1', [id]);
-
-    return res.status(200).json({ detalhesTransacoes });
-    // } catch (error) {
-    //     return res.status(500).json({ mensagem: 'Erro interno.' })
-    // }
 
 }
 
