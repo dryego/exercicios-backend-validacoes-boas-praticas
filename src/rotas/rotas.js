57657b9ed2c691406acdbc57612b8eaee90974a8
dryego barbosa <dryego.lisboa@gmail.com>
2023-02-28 09:36:28 -0300

    Adicionado rota, detalhar uma transação usuario logado.

--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -3,6 +3,7 @@ const { atualizarUsuario } = require('../controladores/atualizarUsuario');
 const { cadastrarTransacaoUsuario } = require('../controladores/cadastrarTransacao');
 const { cadastroUsuario } = require('../controladores/cadastroUsuario');
 const { detralarUsuario } = require('../controladores/detalharUsuario');
+const { detalheTransacaoUsuarioLogado } = require('../controladores/detalheTransacaoUsuarioLogado');
 const { excluirTransacaoUsuario } = require('../controladores/excluirTransacao');
 const login = require('../controladores/fazerLogin');
 const { listarCategorias } = require('../controladores/ListarCategorias');
@@ -20,6 +21,7 @@ rotas.get('/usuario', detralarUsuario);
 rotas.put('/usuario', atualizarUsuario)
 rotas.get('/categoria', listarCategorias)
 rotas.get('/transacao', listarTransacoesUsuario)
+rotas.get('/transacao/:id', detalheTransacaoUsuarioLogado);
 rotas.post('/transacao', cadastrarTransacaoUsuario)
 rotas.delete('/transacao/:id', excluirTransacaoUsuario)
 
