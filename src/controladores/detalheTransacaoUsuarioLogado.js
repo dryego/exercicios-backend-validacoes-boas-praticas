f5d1feb674ac259ad690a5ba99fcaa33a91492dc
dryego barbosa <dryego.lisboa@gmail.com>
2023-02-28 22:13:27 -0300

    coreção detalhamento: nome da categoria.

--- src/controladores/detalheTransacaoUsuarioLogado.js
+++ src/controladores/detalheTransacaoUsuarioLogado.js
@@ -8,19 +8,30 @@ const detalheTransacaoUsuarioLogado = async (req, res) => {
 
         const detalhesTransacoes = await pool.query('select * from transacoes where usuario_id = $1', [usuario.id]);
 
-        const buscaIdTransacao = detalhesTransacoes.rows.find(busca => busca.id == id)
+        const buscaTransacao = detalhesTransacoes.rows.find(buscaId => buscaId.id == id);
 
-        // const buscarIdCategoria = detalhesTransacoes.rows.find(busca => busca.categoria_id);
-
-        if (buscaIdTransacao === undefined) {
+        if (buscaTransacao === undefined) {
             return res.status(400).json({ mensagem: 'Transação não encontrada' });
         }
 
-        return res.status(200).json(buscaIdTransacao);
+        const buscarCategoria = await pool.query('select descricao from categorias where id = $1', [buscaTransacao.categoria_id])
+
+        return res.status(200).json({
+            "id": buscaTransacao.id,
+            "tipo": buscaTransacao.tipo,
+            "descricao": buscaTransacao.descricao,
+            "valor": buscaTransacao.valor,
+            "data": buscaTransacao.data,
+            "usuario_id": buscaTransacao.usuario_id,
+            "categoria_id": buscaTransacao.categoria_id,
+            "categoria_nome": buscarCategoria.rows[0].descricao
+        });
+
 
     } catch (error) {
         return res.status(500).json({ mensagem: 'Erro interno.' })
     }
+
 
 }
 
